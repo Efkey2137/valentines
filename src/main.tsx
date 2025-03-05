79b8d87c147e4f73b647eddaa0b150dbb2165538
Efkey <filipploskonka17@gmail.com>
2025-03-05 19:39:07 +0100

    Add Vercel Analytics dependency and integrate into main component

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { animateTitle } from './titleAnimation';
+import { Analytics } from "@vercel/analytics/react"
 
 // Start the title animation
 animateTitle();
 
 createRoot(document.getElementById('root')!).render(
+  
   <StrictMode>
     <App />
+    <Analytics/>
   </StrictMode>,
 )
